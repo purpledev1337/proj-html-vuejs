f31fc004d992ceac3ec34ab95e299dca81c6ac47
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-12-14 18:48:34 +0100

    Feat: hover on Special Moments (img+text)

--- src/components/SpecialMoments.vue
+++ src/components/SpecialMoments.vue
@@ -4,9 +4,21 @@
     <h1>Baking Special Moments</h1>
     <div class="photo_container">
       <img src="../assets/corporate-bg.jpg" alt="">
+      <img class="hidden" src="../assets/corporate-hover-bg.jpg" alt="">
+      <div class="hidden text">
+        <h6>International Bakery</h6>
+        <h1>Corporate Events</h1>
+        <button>Explore more</button>
+      </div>
     </div>
     <div class="photo_container">
       <img src="../assets/wedding-bg.jpg" alt="">
+      <img class="hidden" src="../assets/wedding-hover-bg.jpg" alt="">
+      <div class="hidden text">
+        <h6>Private Dining Hall</h6>
+        <h1>Weddings & Parties</h1>
+        <button>Explore more</button>
+      </div>
     </div>
   </div>
 </template>
@@ -42,12 +54,44 @@ export default {
   .photo_container {
     margin: 15px auto;
     width: 50%;
+    position: relative;
   
     img {
       width: calc(100% - 100px);
       height: 800px;
       object-fit: cover;
       object-position: left;
+    }
+
+    .text {
+      color: #fff;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+
+      h1 {
+        color: inherit;
+        font-size: 2em;
+        margin: 40px 0;
+
+      }
+
+      button {
+        padding: 10px 15px;
+      }
+    }
+
+    .hidden {
+      display: none;
+    }
+
+    &:hover img {
+      display: none;
+    }
+
+    &:hover .hidden {
+      display: inline-block;
     }
   }
 }
